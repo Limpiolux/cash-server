8c8e2191565a29fe90f1380b9d1d2ca634cf47f7
Micaela Sanchez <msanchez@limpiolux.com.ar>
2024-04-15 14:05:40 -0300

    agrego endpoint para listado de visitas que se filtran por todos los parametros del listado

--- Controllers/VisitaServicioController.cs
+++ Controllers/VisitaServicioController.cs
@@ -1,7 +1,9 @@
 ﻿using cash_server.Data;
 using cash_server.Models;
+using cash_server.SharedKernel;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -12,17 +14,23 @@ namespace cash_server.Controllers
 {
     [RoutePrefix("visitaservicio")]
     [EnableCors(origins: "http://localhost:5173", headers: "*", methods: "*")]
-    public class VisitaServicioController: ApiController
+    public class VisitaServicioController : ApiController
     {
         private readonly VisitaServicioData _visitaServicioData;
         private readonly VisitaServicioFormData _visitaServicioFormData;
         private readonly UsuarioData _usuarioData;
+        private readonly EmpleadoData _empleadoData;
+        private readonly UnidadNegocioData _unidadNegocioData;
+        private readonly ServicioPrestadoData _servicioPrestadoData;
 
         public VisitaServicioController()
         {
             _visitaServicioData = new VisitaServicioData();
             _visitaServicioFormData = new VisitaServicioFormData();
             _usuarioData = new UsuarioData();
+            _empleadoData = new EmpleadoData();
+            _unidadNegocioData = new UnidadNegocioData();
+            _servicioPrestadoData = new ServicioPrestadoData();
         }
 
         //se le envia un json con los datos en el body
@@ -48,7 +56,7 @@ namespace cash_server.Controllers
         [Route("crear")]
         public IHttpActionResult CrearVisitaServicio([FromBody] VisitaServicio visitaServicio)
         {
-           
+
             try
             {
 
@@ -65,7 +73,7 @@ namespace cash_server.Controllers
             }
         }
 
-        //obtiene todas las visitas de todos los usuarios (con sus respectivos form y respuestas)
+        //obtiene todas las visitas de TODOS los usuarios (con sus respectivos form y respuestas)
         //si no hay ninguna visita retorna un array vacio
         [HttpGet]
         [Route("visitasgetall")]
@@ -94,6 +102,7 @@ namespace cash_server.Controllers
                 return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
             }
         }
+
         //este endpoint se podria usar que dado un UsuarioId preventor, traer los datos de sus visitas
         [HttpGet]
         [Route("visitasgetallbyId/{usuarioId}")]
@@ -123,7 +132,7 @@ namespace cash_server.Controllers
                 }
                 else
                 {
-                    return Content(HttpStatusCode.NoContent, "No hay visitas cargadas para el Usuario dado.");
+                    return Content(HttpStatusCode.BadRequest, "No hay visitas cargadas para el Usuario dado.");
                 }
             }
             catch (Exception)
@@ -131,27 +140,114 @@ namespace cash_server.Controllers
                 return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
             }
         }
-
-        //endpoint para filtro fecha
+        /********************************/
+        //ESTE EN REALIAD NO SE VA A USAR
+        //endpoint para filtrar visitas por supervisor
         [HttpGet]
-        [Route("visitasfiltro")]
-        public IHttpActionResult ObtenerVisitasPorFechas(DateTime? fechaInicio, DateTime? fechaFin)
+        [Route("visitasgetallbysupervisor/{supervisorId}")]
+        public IHttpActionResult ObtenerVisitasPorSupervisor(int supervisorId)
         {
             try
             {
-                var visitas = _visitaServicioData.List().ToList(); // Convertimos IQueryable a List
+                var visitas = _visitaServicioData.List();
 
-                if (fechaInicio != null && fechaFin != null)
+                if (!visitas.Any())
                 {
-                    visitas = visitas.Where(v => v.FechaVisita >= fechaInicio && v.FechaVisita <= fechaFin).ToList();
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay visitas cargadas." });
                 }
-                else if (fechaInicio != null)
+
+                var supervisores = _empleadoData.List();
+                
+                var supervisor = supervisores.FirstOrDefault(s => s.Id == supervisorId && s.Activo && s.Rol == RolEmpleado.Supervisor);
+
+                if (supervisor == null)
                 {
-                    visitas = visitas.Where(v => v.FechaVisita >= fechaInicio).ToList();
+                    return Content(HttpStatusCode.NotFound, new { message = "El Supervisor con el Id proporcionado no existe o no está activo" });
                 }
-                else if (fechaFin != null)
+
+                var visitasPorSupervisor = visitas.Where(v => v.SupervisorId == supervisorId).ToList();
+
+                if (visitasPorSupervisor.Any())
                 {
-                    visitas = visitas.Where(v => v.FechaVisita <= fechaFin).ToList();
+                    return Ok(visitasPorSupervisor);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.NotFound, new { message = "No hay visitas para el Supervisor proporcionado" });
+                }
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
+            }
+        }
+    
+        //EN REALIDAD NO SE SI ESTE SE VA A UTILIZAR
+        //endpoint que devuelve las visitas de los preventores
+        [HttpGet]
+        [Route("visitasgetallbypreventores")]
+        public IHttpActionResult ObtenerVisitasPorPreventores()
+        {
+            try
+            {
+                //traes los preventores
+                var preventores = _empleadoData.List().Where(u => u.Rol == RolEmpleado.Preventor && u.Activo);
+
+                if (!preventores.Any())
+                {
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay preventores cargados." });
+                }
+
+                var visitas = new List<VisitaServicio>();
+
+                //recorro y busco la visitas que hayan sido gestionadas por el usuario preventor --> idusuario = idpreventor
+                foreach (var preventor in preventores)
+                {
+                    var visitasPreventor = _visitaServicioData.List().Where(v => v.UsuarioId == preventor.Id);
+                    
+                    visitas.AddRange(visitasPreventor);
+                }
+                
+                if (visitas.Any())
+                {
+                    foreach (var visita in visitas)
+                    {
+                        var formularios = _visitaServicioFormData.List().Where(f => f.VisitaId == visita.Id).ToList();
+                        visita.Formularios = formularios;
+                    }
+                    return Ok(visitas);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay visitas cargadas de los preventores." });
+                }
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
+            }
+        }
+        //EN REALIDAD ESTE NO SE VA A USAR
+        [HttpGet]
+        [Route("visitasgetallbyrol/{rol}")]
+        public IHttpActionResult ObtenerVisitasPorRol(RolUsuario rol) //rol Preventor o Administrador
+        {
+            try
+            {
+                var usuarios = _usuarioData.List().Where(u => u.Rol == rol && u.Activo);
+
+                if (!usuarios.Any())
+                {
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay usuarios con el rol especificado cargados." });
+                }
+
+                var visitas = new List<VisitaServicio>();
+
+                //recorro los usuarios preventores o administradores, y busco las visitas que haya cargado ese usuario
+                foreach (var usuario in usuarios)
+                {
+                    var visitasUsuario = _visitaServicioData.List().Where(v => v.UsuarioId == usuario.Id);
+                    visitas.AddRange(visitasUsuario);
                 }
 
                 if (visitas.Any())
@@ -165,7 +261,7 @@ namespace cash_server.Controllers
                 }
                 else
                 {
-                    return Content(HttpStatusCode.NoContent, "No hay visitas en el rango de fechas proporcionado.");
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay visitas cargadas para el rol especificado." });
                 }
             }
             catch (Exception)
@@ -173,6 +269,134 @@ namespace cash_server.Controllers
                 return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
             }
         }
+
+        /********************/
+
+        //endpoint para filtrar por todos los parametros, del listado de visitas
+        [HttpGet]
+        [Route("filtrar")]
+        public IHttpActionResult FiltrarVisitas(
+            int? unidadNegocioId = null,
+            int? servicioPrestadoId = null,
+            DateTime? fechaDesde = null,
+            DateTime? fechaHasta = null,
+            int? supervisorId = null,
+            int? usuarioId = null,
+            RolUsuario? rolUsuario = null)
+        {
+            try
+            {
+                if (unidadNegocioId != null)
+                {
+                    var unidadNegocio = _unidadNegocioData.GetById(unidadNegocioId.Value);
+                    
+                    if (unidadNegocio == null)
+                    {
+                        return Content(HttpStatusCode.BadRequest, new { message = "La unidad de negocio especificada no existe." });
+                    }
+                }
+
+                if (servicioPrestadoId != null)
+                {
+                    var servicioPrestado = _servicioPrestadoData.GetById(servicioPrestadoId.Value);
+                    
+                    if (servicioPrestado == null)
+                    {
+                        return Content(HttpStatusCode.BadRequest, new { message = "El servicio prestado no existe." });
+                    }
+                }
+                //obtengo las visitas
+                var visitas = _visitaServicioData.List();
+
+                if (!visitas.Any())
+                {
+                    return Content(HttpStatusCode.BadRequest, new { message = "No hay visitas cargadas." });
+                }
+
+                //valido que el supervisorId sea valido
+                var supervisores = _empleadoData.List();
+
+                var supervisor = supervisores.FirstOrDefault(s => s.Id == supervisorId || s.Activo && s.Rol == RolEmpleado.Supervisor);
+
+                if (supervisor == null)
+                {
+                    return Content(HttpStatusCode.NotFound, new { message = "El Supervisor con el Id proporcionado no existe o no está activo" });
+                }
+
+                //valido que el usuarioId sea valido
+                var usuarios = _usuarioData.List();
+
+                var usuario = usuarios.FirstOrDefault(u => u.Id == usuarioId && u.Activo);
+
+                if (usuario == null)
+                {
+                    return Content(HttpStatusCode.NotFound, new { message = "El Usuario con el Id proporcionado no existe o no está activo" });
+                }
+                //validoque el rol de usuario sea valido
+                if (rolUsuario != null && (rolUsuario != RolUsuario.Preventor && rolUsuario != RolUsuario.Administrador))
+                {
+                    return Content(HttpStatusCode.BadRequest, new { message = "El Rol de Usuario especificado no es válido." });
+                }
+                //filtro por unidadde negocio
+                if (unidadNegocioId != null)
+                {
+                    visitas = visitas.Where(v => v.UnidadNegocioId == unidadNegocioId);
+                }
+                //filtro por servicio prestado
+                if (servicioPrestadoId != null)
+                {
+                    visitas = visitas.Where(v => v.ServicioPrestadoId == servicioPrestadoId);
+                }
+                //filtro por fechas
+                if (fechaDesde != null && fechaHasta != null)
+                {
+                    visitas = visitas.Where(v => v.FechaVisita >= fechaDesde && v.FechaVisita <= fechaHasta).ToList();
+                }
+                else if (fechaDesde != null)
+                {
+                    visitas = visitas.Where(v => v.FechaVisita >= fechaDesde).ToList();
+                }
+                else if (fechaHasta != null)
+                {
+                    visitas = visitas.Where(v => v.FechaVisita <= fechaHasta).ToList();
+                }
+                //filtro por supervisor
+                if (supervisorId != null)
+                {
+                    visitas = visitas.Where(v => v.SupervisorId == supervisorId);
+                }
+                //filtro por usuario
+                if (usuarioId != null)
+                {
+                    visitas = visitas.Where(v => v.UsuarioId == usuarioId);
+                }
+
+                if (rolUsuario != null)
+                {
+                    visitas = visitas.Where(v => v.Usuario.Rol == rolUsuario);
+                }
+
+                //si hay alguna visita cargo los formularios de la visita
+                if (visitas.Any())
+                {
+                    foreach (var visita in visitas)
+                    {
+                        var formularios = _visitaServicioFormData.List().Where(f => f.VisitaId == visita.Id).ToList();
+                        visita.Formularios = formularios;
+                    }
+                    return Ok(visitas);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.NotFound, new { message = "No se encontraron visitas que coincidan con los filtros proporcionados." });
+                }
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.InternalServerError, new { error = "Error interno del servidor" });
+            }
+        }
+
 
 
     }
