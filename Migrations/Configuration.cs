268a1ad89c4ac9050faaa60e73d0da9a18749789
msanchez <msanchez@NB-292>
2024-08-15 14:57:34 -0300

    agrego preventores

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -564,7 +564,11 @@
                 new Empleado { Nombre = "María Luz Irassar", Email = "luz_irassar@hotmail.com", Rol = RolEmpleado.Preventor, Activo = true },
                 new Empleado { Nombre = "Yamila Guerrero", Email = "yamiguerrero2008@hotmail.com", Rol = RolEmpleado.Preventor, Activo = true },
                 new Empleado { Nombre = "Sebastian Lescano", Email = "sebastianlesscano28@gmail.com", Rol = RolEmpleado.Preventor, Activo = true },
-                new Empleado { Nombre = "Juan Carlos Daniel Garay", Email = "juancarlosdanielgaray@gmail.com", Rol = RolEmpleado.Preventor, Activo = true }
+                new Empleado { Nombre = "Juan Carlos Daniel Garay", Email = "juancarlosdanielgaray@gmail.com", Rol = RolEmpleado.Preventor, Activo = true },
+                /*ultimos agregados*/
+                new Empleado { Nombre = "Daiana Fabre", Email = "daianaelifabre@gmail.com", Rol = RolEmpleado.Preventor, Activo = true },
+                new Empleado { Nombre = "Tomas Rodriguez", Email = "Hystomasrodriguez@gmail.com", Rol = RolEmpleado.Preventor, Activo = true },
+                new Empleado { Nombre = "Karla Briceño", Email = "karlabriceo53@gmail.com", Rol = RolEmpleado.Preventor, Activo = true },
 
             };
 
